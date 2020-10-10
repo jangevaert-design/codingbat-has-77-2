8580c258abd0ba1d2fda7fea9eb2153e349594cd
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-10 09:07:46 -0600

    Initial commit

---
+++ src/Has77_2.java
@@ -0,0 +1,14 @@
+public class Has77_2 {
+  public boolean has77(int[] nums) {
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (nums[i] == 7 && (nums[i + 1] == 7 || nums[i + 2] == 7)) {
+        return true;
+      }
+      if (nums[i + 1] == 7 && nums[i + 2] == 7) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
